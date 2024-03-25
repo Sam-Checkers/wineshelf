bf5f1919588b50533d1b8511eaf432824b1e70ac
Sam-Checkers <samcollins713@gmail.com>
2024-03-24 23:13:59 -0400

    first commit

--- src/auth0-config.js
+++ src/auth0-config.js
@@ -3,7 +3,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const Auth0ProviderConfig = {
   domain: 'dev-44w2oefzt0a2lc21.us.auth0.com',
   clientId: 'znxWjP8amyuhWvZO3P3PpIkUgWHjuEEc',
-  redirectUri: 'http://localhost:3000'
+  redirectUri: 'https://ubiquitous-salamander-74a514.netlify.app'
 };
 
 export default Auth0ProviderConfig;
